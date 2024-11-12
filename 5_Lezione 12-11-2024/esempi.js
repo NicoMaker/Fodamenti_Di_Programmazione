dffb670848a5b0a4d51530d3ad139b0a3a29662d
Nico Maker <Nicola.marano02gmail.com>
2024-11-12 17:35:12 +0100

    add 5 exercise of js

--- 5_Lezione 12-11-2024/esempi.js
+++ 5_Lezione 12-11-2024/esempi.js
@@ -23,6 +23,19 @@ function quartoesercizio() {
   }
 }
 
+function quintoesercizio() {
+  let somma = 0;
+
+  while (true) {
+    let numero = prompt("inserisci un numero valido 0 non valido");
+    numero = parseFloat(numero);
+
+    if (numero === 0) break;
+
+    if (somma > numero) somma += numero;
+  }
+}
+
 const primoesercizioarrow = () => {
     let x = 5;
     x += 2;
@@ -42,5 +55,17 @@ const primoesercizioarrow = () => {
     while (i < 5) {
       console.log(`Intestazione numero ${i}`);
       i++;
+    }
+  },
+  quintoesercizioarrow = () => {
+    let somma = 0;
+
+    while (true) {
+      let numero = prompt("inserisci un numero valido 0 non valido");
+      numero = parseFloat(numero);
+
+      if (numero === 0) break;
+
+      if (somma > numero) somma += numero;
     }
   };
