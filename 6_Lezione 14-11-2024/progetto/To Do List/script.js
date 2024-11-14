a161aeb8f2ded8cd9a6ae1da707486d8a35d4e6c
Nico Maker <Nicola.marano02gmail.com>
2024-11-14 12:57:58 +0100

    add js progetto

---
+++ 6_Lezione 14-11-2024/progetto/To Do List/script.js
@@ -0,0 +1,61 @@
+function DisplayCurrentdata() {
+    const currentdate = new Date();
+    document.getElementById("current-date").textContent =
+      currentdate.toLocaleDateString();
+  }
+  
+  DisplayCurrentdata();
+  
+  const taskinput = document.getElementById("task-input"),
+    addtaskbtn = document.getElementById("add-task-btn"),
+    tasklist = document.getElementById("task-list"),
+    remainingtask = document.getElementById("remaining-task");
+  
+  let taskcount = 0;
+  
+  function AddTask() {
+    const tasktext = taskinput.value.trim();
+    if (tasktext === "") return;
+  
+    const taskitem = document.createElement("li");
+    taskitem.classList.add("task-item");
+    taskitem.textContent = tasktext;
+
+    taskitem.addEventListener("click", () => RemoveTask(taskitem));
+  
+    const removebtn = document.createElement("button");
+    removebtn.textContent = "rimuovi";
+    taskitem.appendChild(removebtn);
+  
+    tasklist.appendChild(taskitem);
+  
+    taskinput.value = "";
+    taskcount++;
+    UpdateRemainingTasks();
+  }
+  
+  function RemoveTask(taskitem) {
+    tasklist.removeChild(taskitem);
+    taskcount--;
+    UpdateRemainingTasks();
+  }
+  
+  function ToggleTaskComplete(taskitem) {
+    taskitem.classList.toggle("completed");
+  }
+  
+  function UpdateRemainingTasks() {
+    const incompleteTasks = document.querySelectorAll(
+      ".task-item:not(.completed)"
+    ).length;
+    remainingtask.textContent = incompleteTasks;
+  }
+
+  addtaskbtn.addEventListener("click", AddTask);
+  
+  taskinput.addEventListener("keypress", (event) => {
+    if (event.key === "Enter") {
+      AddTask();
+    }
+  });
+  
