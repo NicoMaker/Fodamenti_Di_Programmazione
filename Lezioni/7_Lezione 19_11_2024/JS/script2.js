4991692320a1246d7f339c9a1dd1fc7d6309b662
Nico Maker <Nicola.marano02gmail.com>
2024-11-19 13:59:29 +0100

    add code

--- Lezioni/7_Lezione 19_11_2024/JS/script2.js
+++ Lezioni/7_Lezione 19_11_2024/JS/script2.js
@@ -1,5 +1,17 @@
-// Lista dei colori di sfondo const colors= ["#FF5733", "#33FF57" , "#3357FF" 
-"#F333FF", "#FFFF33", "#FF5733", "#33FFF7"]; 
-// Funzione per cambiare 1l colore di sfondo function changeBackgroundColor() ( // Seleziona un colore casuale dalla lista const randomColor colors[Math.floor(Math.random() * colors.length)]; // Cambia il colore di sfondo della pagina document.body.style.backgroundColor = randomColor; 
-// Aggiungi un listener al bottone per cambiare il colore document.getElementById("colorButton").addEventListener("click", changeBackgroundColor); 
-qGe
+const colors = [
+  "#ff5733",
+  "#333f57",
+  "#33357ff",
+  "#ff333ff",
+  "#ffff33",
+  "#ff5733",
+  "#33fff7",
+];
+function changebackroundcolor() {
+  const randomcolor = colors[Math.floor(Math.random() * colors.length)];
+  document.body.style.backgroundColor = randomcolor;
+}
+
+document
+  .getElementById("colorButton")
+  .addEventListener("click", changebackroundcolor);
